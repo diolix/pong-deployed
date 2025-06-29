e53e21ebeab52d6eb7b5c6f503ed9ed0e9e973c1
William Stefini <wstefini@gmail.com>
2025-06-29 10:13:09 +0200

    Premier commit

---
+++ out/matchbox-test_bg.wasm.d.ts
@@ -0,0 +1,19 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const main: (a: number, b: number) => number;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_export_1: WebAssembly.Table;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_export_6: WebAssembly.Table;
+export const closure6979_externref_shim: (a: number, b: number, c: any) => void;
+export const closure7087_externref_shim: (a: number, b: number, c: any) => void;
+export const _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h706c130cc7a37fed: (a: number, b: number) => void;
+export const closure8975_externref_shim: (a: number, b: number, c: any) => void;
+export const _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h00f700f5ee2013fd: (a: number, b: number) => void;
+export const closure8978_externref_shim: (a: number, b: number, c: any, d: any) => void;
+export const closure100376_externref_shim: (a: number, b: number, c: any) => void;
+export const __wbindgen_start: () => void;
